d2f0df884ce659b2ff3ce962ede23389fa941372
anselmoncy97 <ansel.moncy97@gmail.com>
2023-03-22 12:01:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import profile from './img/propic.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <h2>test run</h2>
+      <img src={profile} alt="propic"/>
+    </div>
+  );
+}
+
+export default App;
